9eaa5e5fc628c6589abb0a657dcdfd896dc68721
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-12-21 13:02:52 +0900

    [update] headerのcssの変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -45,16 +45,17 @@ header {
   top: 0;
   height: 60px;
   width: 100%;
+  border-bottom: 2px solid #abbdc4;
 }
 header .logo {
   float: left;
   height: 100%;
+  margin-left: 10px;
 }
 
 header .logo  p{
   font-size: 18px;
   margin-top: 18px;
-  margin-left: 10px;
 }
 header .u-icon{
   float: right;
@@ -72,7 +73,7 @@ header .u-icon img {
 header .u-icon .login-button i {
   color: #55acee;
 }
-span.login-button {
+header .u-icon span.login-button {
   line-height: 45px;
   width: 100px;
   margin: 5px;
@@ -82,12 +83,11 @@ span.login-button {
 	text-decoration: none;
 	color:  #55acee;
 	background:#FFF;
-	border-bottom: 2px solid #55acee;
 	border-radius: 4px;
   border: 2px solid #55acee;
 	box-shadow: inset 0 2px 0 rgba(255,255,255,0.2), 0 2px 2px rgba(0, 0, 0, 0.19);
 }
-span.login-button:active {
+header .u-icon span.login-button:active {
 	border-bottom: 2px solid #55acee;
 	box-shadow: 0 0 2px rgba(0, 0, 0, 0.30);
 }
