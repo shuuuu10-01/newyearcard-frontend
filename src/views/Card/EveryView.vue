eba082400a3eb88618b4b37ce3765f08830c5645
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-12-23 10:55:19 +0900

    [update] cardを作った人も見れるように

--- src/views/Card/EveryView.vue
+++ src/views/Card/EveryView.vue
@@ -33,46 +33,46 @@ export default {
         this.card.uid = response.data.data.uid
         this.card.share = response.data.data.share
         this.isShow = true
+        if(this.get_uid == this.card.uid){
+          return false
+        }
         console.log(this.card.share,"showwww")
         return true
       }).catch(()=>{ //error処理
-        throw new Error('throw Error');
+        alert("カードが存在しません")
+        this.$router.push("/")
+        return false
       })
     },
     async checkUser() {
       if (this.card.share==0) {
         console.log("cU-0-ok")
-        return 'clear'
+        return true
       } else if (this.card.share==1){
         console.log("cU-1")
-        this.checkFriend().then(()=>{
-          console.log("cU-1-ok")
-          return 'clear'
-        }).catch(()=>{
-          console.log("cU-1-no")
-          throw new Error('throw Error');
-        })
+        await this.checkFriend()
+        console.log("")
       } else {
-        this.checkDM().then(result=>{
-          console.log(result)
-          return 'clear'
-        }).catch(()=> {
-          throw new Error('throw Error');
-        })
+        await this.checkDM();
+        
       }
     },
     async checkFriend() { //cardを作成した人のuidとみる人のuidで関係性を確認
+    console.log("startCheck")
       if(this.get_uid !="") {
-        this.axios.get(this.get_api_twitter+this.card.uid+'/'+this.get_uid+'/friendships')
+        await this.axios.get(this.get_api_twitter+this.card.uid+'/'+this.get_uid+'/friendships')
         .then(response => {
           console.log(response)
           if (response.data.relationship.source.followed_by){
-            return 'clear'
+            console.log("cF-true")
+            return true
           } else {
+            console.log("cF-false")
             alert("このページは限定公開に設定されています")
             throw new Error('throw Error');
           }
         }).catch(()=>{
+          console.log("cF-e")
           alert("エラーが発生しました")
         })
       } else {
@@ -83,7 +83,7 @@ export default {
     async checkDM() {
       if(this.get_uid !="") {
         if(this.card.share == this.get_uid) {
-          return 'clear'
+          return true
         } else {
           alert("このページは限定公開に設定されています。")
           throw new Error('throw Error');
@@ -97,11 +97,13 @@ export default {
   mounted: async function() {
     await this.$store.dispatch("auth");
     console.log("auth")
-    await this.showApi();
-    console.log("show")
-    await this.checkUser();
-    console.log("check")
-    },
+    const self = await this.showApi();
+    console.log(self)
+    if (self) {
+      await this.checkUser();
+      console.log("check")
+    }
+  },
   computed: {
     get_api_rails() {
       return this.$store.getters.get_API_URL
