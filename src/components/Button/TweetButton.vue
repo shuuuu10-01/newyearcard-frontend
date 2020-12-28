9ea2a7677f57eda3de6d9f05d22471b8f56dab3f
shuuuu <tanaka.shu642@mail.kyutech.jp>
2020-12-28 11:53:23 +0900

    [update] tweetボタンのバグの改善

--- src/components/Button/TweetButton.vue
+++ src/components/Button/TweetButton.vue
@@ -1,3 +1,24 @@
 <template>
-  <a class="tweet-button twitter-share-button" href="https://twitter.com/intent/tweet" data-text="年賀状をWebアプリで作ってみた" :data-url="'https://newyeaercard.netlify.app/#'+$route.path" data-lang="ja"><i class="fa fa-twitter"></i> ツイート</a>
+  <div>
+    <a class="tweet-button twitter-share-button" href="https://twitter.com/intent/tweet" data-text="年賀状をWebアプリで作ってみた" :data-url="'https://newyeaercard.netlify.app/#'+$route.path" data-lang="ja"><i class="fab fa-twitter"></i> ツイート</a>
+  </div>
 </template>
+
+<style scoped>
+a {
+  position: relative;
+  background-color: #55acee;
+  font-weight: 500;
+  padding: 1px 8px 1px 6px;
+  cursor: pointer;
+  border: 2px solid #55acee;
+  border-radius: 3px;
+  color: white;
+  line-height: 30px;
+  font-size: 12px;
+  text-decoration: none;
+}
+a:hover {
+  background-color:  #4791c9;
+}
+</style>
