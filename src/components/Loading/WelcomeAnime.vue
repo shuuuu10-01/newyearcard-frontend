9d2ff3bf25802076d7ef2a1fa8fb83e28f7ba5ae
shuuuu10-01 <tanaka.shu642@mail.kyutech.jp>
2020-12-29 22:59:35 +0900

    [update]

--- src/components/Loading/WelcomeAnime.vue
+++ src/components/Loading/WelcomeAnime.vue
@@ -10,18 +10,3 @@ export default {
   components: { HappyNewYear },
 }
 </script>
-
-<style scoped>
-.welcome{
-  z-index: 1;
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgb(226, 219, 219);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
